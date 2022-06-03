f4f7b4f4e261f2dff3e6e7e4af8cad86305d6704
yaskin <yaskin@yandex.ru>
2022-06-03 16:59:45 +0300

    Add files via upload

---
+++ wa.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!--Как пользоваться:
+Залить файл wa.php на ваш хостинг.
+Убедитесь, что он доступен по адресу: http://вашдомен/wa.php
+Составьте ссылку, как вы это делаете для wa.me ( http://вашдомен/wa.php?phone=79999999999 ).
+Наслаждайтесь.
+тг @polosok
+-->
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>WA</title>
+</head>
+<body class="" onload="myWhatsappFunction()">
+  <a id="whatsapp-btn" class="" target="_blank">Whatsapp</a>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/mobile-detect/1.4.3/mobile-detect.min.js"></script>
+  <script>
+    var whatsapp_number = "<?php settype($_GET['phone'], 'integer'); echo isset($_GET['phone']) ? (int) $_GET['phone'] : ''; ?>";
+    function myWhatsappFunction() {
+      var md = new MobileDetect(window.navigator.userAgent);
+      if (md.mobile()) {
+        document.getElementById("whatsapp-btn").href = "whatsapp://send?phone=" + whatsapp_number;
+      } else {
+        document.getElementById("whatsapp-btn").href = "https://web.whatsapp.com/send/?phone=" + whatsapp_number;        
+      }
+      location.href = document.getElementById("whatsapp-btn").href;
+    };
+  </script>
+</body>
+</html>
